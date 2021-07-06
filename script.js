fb11989f8d9210f8c67a5787cd8bbd1608cea0fc
Michael Jakobsen <michael@rpair.co.uk>
2021-07-06 17:33:12 +0100

    added javascript and search fuction for new search field

---
+++ script.js
@@ -0,0 +1,17 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
+
+open.addEventListener("click", () => container.classList.add("show-nav"));
+
+close.addEventListener("click", () => container.classList.remove("show-nav"));
+
+// Search Code
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector("input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
