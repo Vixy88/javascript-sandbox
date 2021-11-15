efbd25cf730990768788c36ba9c4b85528006c7f
Michael Jakobsen <michael@rpair.co.uk>
2021-11-15 22:17:19 +0000

    Added a new HTML and css template for a landing page

--- playground/functions.js
+++ playground/functions.js
@@ -1,7 +1,2 @@
-let name;
 
-function greet(name) {
-  document.querySelector('H1').innerText = `Hello ${name}`;
-}
 
-greet('Michael')
---
+++ playground/window-object.js
@@ -0,0 +1,34 @@
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// ALERT
+// alert('Hello World');
+
+// PROMPT
+// const input = prompt();
+// alert(input);
+
+// CONFIRM
+// if(confirm('Are you sure?')) {
+//     console.log('YES');
+// } else { 
+//     console.log('NO');
+// }
+
+let val;
+
+// Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+val = window.location
+
+console.log(val);
